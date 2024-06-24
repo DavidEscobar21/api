94d155b811609f227f60089e447f9a3c2ac4f6be
DavidEscobar21 <davidandrew.de@gmail.com>
2024-06-23 18:05:26 -0600

    commit desde intellij

--- src/main/java/med/voll/api/repository/MedicoRepository.java
+++ src/main/java/med/voll/api/repository/MedicoRepository.java
@@ -13,6 +13,7 @@ import java.time.LocalDateTime;
 public interface MedicoRepository extends JpaRepository<Medico, Long> {
     Page<Medico> findByActivoTrue(Pageable pageable);
 
+    //primer cambio en intellij
     @Query("select m from Medico m where m.activo=1 and m.especialidad=:especialidad and m.id not in(select c.medico.id from Consulta c where c.fecha=:fecha) order by rand() limit 1")
     Medico seleccionarMedicoConEspecialidadEnFecha(Especialidad especialidad, LocalDateTime fecha);
 }
